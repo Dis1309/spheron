686520e615eeaac221477ddf0f521419141a2fe4
maskboyAvi <aviralofficial1729@gmail.com>
2024-10-15 04:08:11 +0530

    Added dashboard project functionality

--- web/src/components/ui/ProjectCard.jsx
+++ web/src/components/ui/ProjectCard.jsx
@@ -1,43 +1,32 @@
 import React from "react";
 import Image from "next/image";
 
-// {
-//     projectId: 10,
-//     imageurl: Projecturl4,
-//     Title: "Card smx",
-//     description: "This is a description for project 10.",
-//     tags: ["ui/ux", "security", "web"],
-//     startDate: "2024-09-18",
-//     endDate: "2024-01-28",
-//     MaxBountyAmount: 8580,
-//     githubLink: "https://github.com/user/project_10",
-//     levels: {
-//       Critical: 5400,
-//       High: 2845,
-//       Low: 615,
-//     },
 const ProjectCard = ({ project }) => {
-  const { imageurl, description } = project;
+  const { imageUrl, description, title, startdate, enddate, maxbounty } = project;
+
   return (
-    <div className="w-full gap-32 py-3 px-10 flex flex-row justify-evenly items-center border rounded-lg hover:scale-105 hover:rounded-xl transition-all duration-200">
-      <Image
-        src={imageurl}
-        alt="Profile Picture"
-        width={70}
-        height={70}
-        className="rounded-full object-cover"
-      />
-      <h1 className="text-lg font-bold text-gray-400">{project.Title}</h1>
+    <div className="flex flex-row justify-around items-center border border-gray-600 rounded-lg p-4 w-[1000px] h-[150px] hover:scale-105 hover:rounded-xl transition-all duration-200"> {/* Set to 900px width */}
+      <div className="relative w-16 h-16"> {/* Fixed width for the image */}
+        <Image
+          src={imageUrl}
+          alt={description.substring(0, 10)}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+          unoptimized
+        />
+      </div>
+      <h1 className="text-lg font-bold text-gray-400">{title}</h1>
       <div className="flex flex-col gap-1">
         <p className="font-light text-white">Start Date</p>
-        <h3 className="text-lg font-md text-gray-200">{project.startDate}</h3>
+        <h3 className="text-lg font-md text-gray-200">{startdate}</h3>
       </div>
       <div className="flex flex-col gap-1">
         <p className="font-light text-white">End Date</p>
-        <h3 className="text-lg font-md text-gray-200">{project.endDate}</h3>
+        <h3 className="text-lg font-md text-gray-200">{enddate}</h3>
       </div>
       <h1 className="text-[1.5rem] font-bold text-gray-200">
-        {"$" + project.MaxBountyAmount}
+        {"$" + maxbounty}
       </h1>
     </div>
   );
